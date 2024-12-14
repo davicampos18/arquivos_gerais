a2eecccafc5fa538a1063d5eb4a2cdb41828ee5c
Davi Silva de Campos <121504097+davicampos18@users.noreply.github.com>
2024-12-14 09:53:18 -0300

    Update Aula6.py
    Finalizado a prova da aula 6 de Python

--- 1 - Infinit School/4 - Aulas Oficiais do Curso/3 - Python AI/Aula6.py
+++ 1 - Infinit School/4 - Aulas Oficiais do Curso/3 - Python AI/Aula6.py
@@ -8,14 +8,14 @@ geracao_id = 1
 def limpar_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def criar_tarefas(id, nome_da_tarefa, descricao, categoria, prioridade, banco_de_dados:list):
+def criar_tarefas(id, nome_da_tarefa, descricao, categoria, prioridade, banco_de_dados:list, status:False):
     banco_de_dados.append({
         "ID":id,
         "Tarefa":nome_da_tarefa,
         "Descrição":descricao,
         "Categoria":categoria,
         "Prioridade":prioridade,
-        "Status":False
+        "Status":f"{"Em andamento" if status == False else "Concluído"}"
     })
 
     print("Tarefa Cadsatrada com Sucesso!")
@@ -28,6 +28,30 @@ def localizar_tarefa(id:int, banco_de_dados:list):
 def excluir_tarfa(id:int, banco_de_dados:list):
     banco_de_dados.pop(id - 1)
     print("Tarefa Excluída com Sucesso!")
+    
+def editar_tarefa(id, item, modificacao, banco_de_dados:list):
+    tarefa = banco_de_dados[id - 1]
+    match item:
+      case 1:
+        tarefa["Nome"] = modificacao
+      case 2:
+        tarefa["Descrição"] = modificacao
+      case 3:
+        tarefa["Categoria"] = modificacao
+      case 4:
+        prioridade_cad = int(input("Selecione o tipo de prioridade:\n1 - Alta\n2 - Média\n3 - Baixa\n"))
+        match prioridade_cad:
+          case 1:
+            modificacao = "Alta"
+          case 2:
+            modificacao = "Média"
+          case 3:
+            modificacao = "Baixa"
+        tarefa["Prioridade"] = modificacao
+      case 5:
+        tarefa["Status"] = "Concluído"
+    print("Tarefa Modificada com Sucesso!")
+  
 
 limpar_tela()
 while True:
@@ -61,7 +85,7 @@ while True:
                     prioridade_cad = "Média"
                 case 3:
                     prioridade_cad = "Baixa"
-            criar_tarefas(id_cad, nome_cad, descricao_cad, categoria, prioridade_cad, banco_de_tarefas)
+            criar_tarefas(id_cad, nome_cad, descricao_cad, categoria, prioridade_cad, banco_de_tarefas, status=False)
             geracao_id += 1
             time.sleep(2)
             limpar_tela()
@@ -70,6 +94,12 @@ while True:
             limpar_tela()
             print(tabulate(banco_de_tarefas, headers="keys", tablefmt="grid"))
             escolha = int(input("Qual o ID da tarefa que deseja editar? "))
+            item = int(input("O que você deseja modificar?\n1 - Nome\n2 - Descriçao\n3 - Categoria\n4 - Prioridade\n5 - Status\n==> "))
+            if item != 5 and item != 4:
+              modificacao = input("Digite a modificação desejada: ")
+            else:
+              modificacao = 0
+            editar_tarefa(escolha, item,modificacao, banco_de_tarefas)
             localizar_tarefa(escolha, banco_de_tarefas)
             time.sleep(1)
             limpar_tela()
@@ -84,6 +114,7 @@ while True:
             limpar_tela()
             print(tabulate(banco_de_tarefas, headers="keys", tablefmt="grid"))
         case 5:
+            print("Encerrando Aplicação....")
+            time.sleep(1)
             limpar_tela()
             break
-
